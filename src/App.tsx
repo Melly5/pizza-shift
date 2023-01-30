f2f434cab6e8ed4a2787bc0d666455cc2ac83fa4
loyal_goose <polina.alina.2015@mail.ru>
2023-01-30 11:53:54 +0700

    feat: добавить header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { RootPage } from './pages/root';
 import { ROUTES } from './utils/constants/router';
+import { PizzaList } from './pages/pizzaList';
 
 const router = createBrowserRouter([
   {
     path: ROUTES.ROOT,
     element: <RootPage />
+  },
+  {
+    path: ROUTES.PIZZA_LIST,
+    element: <PizzaList />
   }
 ]);
 
---
+++ src/components/header/header.tsx
@@ -0,0 +1,22 @@
+import '../../assets/scss/header.scss';
+import { Link, useNavigate } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <div className='main'>
+      <p className='name'>SANTORINIZ</p>
+      <div className='image'>pepper</div>
+      <div className='variants'>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <p>Главная</p>
+        </Link>
+        <Link to='/pizzas' style={{ textDecoration: 'none' }}>
+          <p>Выбрать пиццу</p>
+        </Link>
+        <Link to='/pizzas' style={{ textDecoration: 'none' }}>
+          <p>Оформить заказ</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
