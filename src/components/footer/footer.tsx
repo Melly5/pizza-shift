291b1f8a0c316ffccbea6efd23bbd528ceb338b1
loyal_goose <polina.alina.2015@mail.ru>
2023-01-31 21:52:52 +0700

    feat добавить иконки footer'у

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,8 +3,15 @@ import './footer.scss';
 export const Footer = () => {
   return (
     <div className='main-footer'>
-      <p className='name'>SANTORINIZ</p>
-      <p className='copyright'>Copyright © 2000 - 2023</p>
+      <div className='content'>
+        <p className='name'>SANTORINIZ</p>
+        <p className='copyright'>Copyright © 2000 - 2023</p>
+      </div>
+      <div className='content'>
+        <img src='files/footer/facebook.svg' alt='facebook' />
+        <img src='files/footer/twitter.svg' alt='twitter' />
+        <img src='files/footer/instagram.svg' alt='instagram' />
+      </div>
     </div>
   );
 };
