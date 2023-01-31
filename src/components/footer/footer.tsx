b7817e179d2cfe03f60c1ce91b833e09e0513356
loyal_goose <polina.alina.2015@mail.ru>
2023-01-31 11:03:48 +0700

    feat: добавить footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import './footer.scss';
+
+export const Footer = () => {
+  return (
+    <div className='main-footer'>
+      <p className='name'>SANTORINIZ</p>
+      <p className='copyright'>Copyright © 2000 - 2023</p>
+    </div>
+  );
+};
